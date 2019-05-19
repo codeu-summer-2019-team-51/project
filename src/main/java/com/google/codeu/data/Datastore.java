8c7c1e7feb870c2c96c551e8d4ab31c42097eafd
archthegit <archieprad@gmail.com>
2019-05-19 19:26:48 +0200

    made requested changes

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -23,81 +23,95 @@ import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.SortDirection;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-/** Provides access to the data stored in Datastore. */
+/**
+ * Provides access to the data stored in Datastore.
+ */
 public class Datastore {
 
-  private DatastoreService datastore;
+    private DatastoreService datastore;
 
-  public Datastore() {
-    datastore = DatastoreServiceFactory.getDatastoreService();
-  }
-
-  /** Stores the Message in Datastore. */
-  public void storeMessage(Message message) {
-    Entity messageEntity = new Entity("Message", message.getId().toString());
-    messageEntity.setProperty("user", message.getUser());
-    messageEntity.setProperty("text", message.getText());
-    messageEntity.setProperty("timestamp", message.getTimestamp());
-
-    datastore.put(messageEntity);
-  }
-
-  /**
-   * Gets messages posted by a specific user.
-   *
-   * @return a list of messages posted by the user, or empty list if user has never posted a
-   *     message. List is sorted by time descending.
-   */
-  public List<Message> getMessages(String user) {
-    List<Message> messages;
-
-    Query query =
-            new Query("Message")
-                    .setFilter(new Query.FilterPredicate("user", FilterOperator.EQUAL, user))
-                    .addSort("timestamp", SortDirection.DESCENDING);
-    PreparedQuery results = datastore.prepare(query);
-
-    messages = createList(results, user);
-    return messages;
-
-  }
-
-  public List<Message> getAllMessages(){
-    List<Message> messages;
-
-    Query query = new Query("Message").addSort("timestamp", SortDirection.DESCENDING);
-    PreparedQuery results = datastore.prepare(query);
-
-    messages = createList(results,null);
-    return messages;
-
-  }
-
-
-  public List<Message> createList(PreparedQuery results,String user) {
-    List<Message> messages = new ArrayList<>();
-    for (Entity entity : results.asIterable()) {
-      try {
-        String idString = entity.getKey().getName();
-        UUID id = UUID.fromString(idString);
-        String temp = user==null?(String) entity.getProperty("user"):user;
-        String text = (String) entity.getProperty("text");
-        long timestamp = (long) entity.getProperty("timestamp");
-
-        Message message = new Message(id, temp, text, timestamp);
-        messages.add(message);
-      } catch (Exception e) {
-        System.err.println("Error reading message.");
-        System.err.println(entity.toString());
-        e.printStackTrace();
-      }
+    public Datastore() {
+        datastore = DatastoreServiceFactory.getDatastoreService();
     }
 
-    return messages;
-  }
+    /**
+     * Stores the Message in Datastore.
+     */
+    public void storeMessage(Message message) {
+        Entity messageEntity = new Entity("Message", message.getId().toString());
+        messageEntity.setProperty("user", message.getUser());
+        messageEntity.setProperty("text", message.getText());
+        messageEntity.setProperty("timestamp", message.getTimestamp());
+
+        datastore.put(messageEntity);
+    }
+
+    /**
+     * Gets messages posted by a specific user.
+     *
+     * @return a list of messages posted by the user, or empty list if user has never posted a
+     * message. List is sorted by time descending.
+     */
+    public List<Message> getMessages(String user) {
+        List<Message> messages;
+
+        Query query =
+                new Query("Message")
+                        .setFilter(new Query.FilterPredicate("user", FilterOperator.EQUAL, user))
+                        .addSort("timestamp", SortDirection.DESCENDING);
+        PreparedQuery results = datastore.prepare(query);
+
+        return createList(results, user);
+    }
+
+    /**
+     * Gets messages posted by all users.
+     *
+     * @return a list of all messages posted. List is sorted by time descending.
+     */
+    public List<Message> getAllMessages() {
+        List<Message> messages;
+
+        Query query = new Query("Message").addSort("timestamp", SortDirection.DESCENDING);
+        PreparedQuery results = datastore.prepare(query);
+
+        return createList(results, null);
+
+
+    }
+
+
+    /**
+     * Creates a list of messages either with user id or without.
+     *
+     * @return a list of all messages posted or by a specific user
+     */
+
+    public List<Message> createList(PreparedQuery results, String user) {
+        List<Message> messages = new ArrayList<>();
+        for (Entity entity : results.asIterable()) {
+            try {
+                String idString = entity.getKey().getName();
+                UUID id = UUID.fromString(idString);
+                String temp = user == null ? (String) entity.getProperty("user") : user;
+                String text = (String) entity.getProperty("text");
+                long timestamp = (long) entity.getProperty("timestamp");
+
+                Message message = new Message(id, temp, text, timestamp);
+                messages.add(message);
+            } catch (Exception e) {
+                System.err.println("Error reading message.");
+                System.err.println(entity.toString());
+                e.printStackTrace();
+            }
+        }
+
+        return messages;
+    }
 
 }
--- src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
@@ -1,16 +1,24 @@
 package com.google.codeu.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+/**
+ * This servlet will respond with a hard-coded "this will be my message feed"
+ * whenever the user requests the /feed URL.
+ */
+
 @WebServlet("/feed")
 public class MessageFeedServlet extends HttpServlet {
 
+
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.getOutputStream().println("this will be my message feed!");
     }
 
