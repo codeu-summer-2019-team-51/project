e75464efbce6fcc502a401bbdaa663f296ad1a46
Karen Frilya Celine <karenfrilya97@gmail.com>
2019-05-27 22:03:02 +0800

    Fix indentation errors in AboutMeServlet

--- src/main/java/com/google/codeu/servlets/AboutMeServlet.java
+++ src/main/java/com/google/codeu/servlets/AboutMeServlet.java
@@ -8,15 +8,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Responds with a hard-coded message for testing purposes.
- */
+* Responds with a hard-coded message for testing purposes.
+*/
 @WebServlet("/about")
 public class AboutMeServlet extends HttpServlet{
 
- @Override
- public void doGet(HttpServletRequest request, HttpServletResponse response)
-   throws IOException {
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
 
-  response.getOutputStream().println("hello world");
- }
+    response.getOutputStream().println("hello world");
+  }
 }
