b9ae184f0a75b88ddd7ccdb0103ca3434193cb85
archthegit <archieprad@gmail.com>
2019-05-28 22:21:18 +0200

    resolving stickler issues

--- src/main/webapp/js/feed.js
+++ src/main/webapp/js/feed.js
@@ -17,32 +17,32 @@ function fetchMessages(){
 }
 
 function buildMessageDiv(message){
- const usernameDiv = document.createElement('div');
- usernameDiv.classList.add("left-align");
- usernameDiv.appendChild(document.createTextNode(message.user));
+  const usernameDiv = document.createElement('div');
+  usernameDiv.classList.add("left-align");
+  usernameDiv.appendChild(document.createTextNode(message.user));
 
- const timeDiv = document.createElement('div');
- timeDiv.classList.add('right-align');
- timeDiv.appendChild(document.createTextNode(new Date(message.timestamp)));
+  const timeDiv = document.createElement('div');
+  timeDiv.classList.add('right-align');
+  timeDiv.appendChild(document.createTextNode(new Date(message.timestamp)));
 
- const headerDiv = document.createElement('div');
- headerDiv.classList.add('message-header');
- headerDiv.appendChild(usernameDiv);
- headerDiv.appendChild(timeDiv);
+  const headerDiv = document.createElement('div');
+  headerDiv.classList.add('message-header');
+  headerDiv.appendChild(usernameDiv);
+  headerDiv.appendChild(timeDiv);
 
- const bodyDiv = document.createElement('div');
- bodyDiv.classList.add('message-body');
- bodyDiv.appendChild(document.createTextNode(message.text));
+  const bodyDiv = document.createElement('div');
+  bodyDiv.classList.add('message-body');
+  bodyDiv.appendChild(document.createTextNode(message.text));
 
- const messageDiv = document.createElement('div');
- messageDiv.classList.add("message-div");
- messageDiv.appendChild(headerDiv);
- messageDiv.appendChild(bodyDiv);
+  const messageDiv = document.createElement('div');
+  messageDiv.classList.add("message-div");
+  messageDiv.appendChild(headerDiv);
+  messageDiv.appendChild(bodyDiv);
 
- return messageDiv;
+  return messageDiv;
 }
 
 // Fetch data and populate the UI of the page.
 function buildUI(){
- fetchMessages();
+  fetchMessages();
 }
