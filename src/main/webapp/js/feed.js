c046344491bb5e0e26ed174143b9f2d0ce7267cd
archthegit <archieprad@gmail.com>
2019-05-28 23:37:17 +0200

    resolved issues with html not rendering right

--- src/main/webapp/js/feed.js
+++ src/main/webapp/js/feed.js
@@ -1,24 +1,6 @@
-// Fetch messages and add them to the page.
-function fetchMessages(){
-  const url = '/feed';
-  fetch(url).then((response) => {
-    return response.json();
-  }).then((messages) => {
-    const messageContainer = document.getElementById('message-container');
-    if(messages.length === 0) {
-     messageContainer.innerHTML = '<p>There are no posts yet.</p>';
-    } else {
-     messageContainer.innerHTML = '';
-    } messages.forEach((message) => {
-    const messageDiv = buildMessageDiv(message);
-     messageContainer.appendChild(messageDiv);
-    });
-  });
-}
-
-function buildMessageDiv(message){
+function buildMessageDiv(message) {
   const usernameDiv = document.createElement('div');
-  usernameDiv.classList.add("left-align");
+  usernameDiv.classList.add('left-align');
   usernameDiv.appendChild(document.createTextNode(message.user));
 
   const timeDiv = document.createElement('div');
@@ -35,11 +17,31 @@ function buildMessageDiv(message){
   bodyDiv.appendChild(document.createTextNode(message.text));
 
   const messageDiv = document.createElement('div');
-  messageDiv.classList.add("message-div");
+  messageDiv.classList.add('message-div');
   messageDiv.appendChild(headerDiv);
   messageDiv.appendChild(bodyDiv);
 
   return messageDiv;
+}
+
+// Fetch messages and add them to the page.
+function fetchMessages() {
+  const url = '/feed';
+  fetch(url).then((response) => {
+    return response.json();
+  }).then((messages) => {
+    const messageContainer = document.getElementById('message-container');
+    if(messages.length == 0) {
+      messageContainer.innerHTML = '<p>There are no posts yet.</p>';
+    }
+    else {
+      messageContainer.innerHTML = '';
+    }
+    messages.forEach((message) => {
+      const messageDiv = buildMessageDiv(message);
+      messageContainer.appendChild(messageDiv);
+    });
+  });
 }
 
 // Fetch data and populate the UI of the page.
