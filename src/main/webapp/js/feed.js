a9096986d47be11b85adaef8c7327c44206463c2
archthegit <archieprad@gmail.com>
2019-05-29 01:23:33 +0200

    stickler issues

--- src/main/webapp/js/feed.js
+++ src/main/webapp/js/feed.js
@@ -27,8 +27,8 @@ function buildMessageDiv(message) {
 // Fetch messages and add them to the page.
 function fetchMessages() {
   const url = '/feed';
-  fetch(url).then((response) => return response.json();
-  ).then((messages) => {
+  fetch(url).then((response) => { return response.json(); })
+  .then((messages) => {
     const messageContainer = document.getElementById('message-container');
     if(messages.length === 0) {
       messageContainer.innerHTML = '<p>There are no posts yet.</p>';
