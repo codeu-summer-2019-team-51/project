c2e5cdc50b85786e397b6cd9f7fa8441291e19e1
archthegit <archieprad@gmail.com>
2019-05-29 21:05:31 +0200

    resolving stickler

--- src/main/webapp/js/feed.js
+++ src/main/webapp/js/feed.js
@@ -27,19 +27,21 @@ function buildMessageDiv(message) {
 // Fetch messages and add them to the page.
 function fetchMessages() { // eslint-disable-line-no-unused-vars
   const url = '/feed';
-  fetch(url).then((response) =>
-    return response.json()).then((messages) => {
-    const messageContainer = document.getElementById('message-container');
-    if (messages.length == 0) {
-      messageContainer.innerHTML = '<p>There are no posts yet.</p>';
-    } else {
-      messageContainer.innerHTML = '';
-    }
-    messages.forEach((message) => {
-      const messageDiv = buildMessageDiv(message);
-      messageContainer.appendChild(messageDiv);
+  fetch(url)
+    .then((response) =>
+      return response.json())
+    .then((messages) => {
+      const messageContainer = document.getElementById('message-container');
+      if (messages.length == 0) {
+        messageContainer.innerHTML = '<p>There are no posts yet.</p>';
+      } else {
+        messageContainer.innerHTML = '';
+      }
+      messages.forEach((message) => {
+        const messageDiv = buildMessageDiv(message);
+        messageContainer.appendChild(messageDiv);
+      });
     });
-  });
 }
 
 // Fetch data and populate the UI of the page.
