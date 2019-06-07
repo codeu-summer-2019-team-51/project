726db3197fa6b115f2203b48337248b4d2580e62
jannatbedi <jannatbedi@gmail.com>
2019-06-07 16:11:43 +0800

    Added Book.java

--- src/main/java/com/google/codeu/data/Book.java
+++ src/main/java/com/google/codeu/data/Book.java
@@ -3,18 +3,17 @@ package com.google.codeu.data;
 import java.util.List;
 
 public class Book {
-
 	private final Int id;
 	private String title;
 	private String[] authors;
 	private List<Int> ratings;
 	private List<String> reviews;
 	private String coverPic;
-	private List<String> morePics;//required or not?
+	private List<String> morePics;
 
 	/*Creates a new Book object for every new book.*/
 	public Book(Int id, String title, String[] authors) {
-		this.id = id;
+	  this.id = id;
 		this.title = title;
 		this.authors = authors;
 	}
