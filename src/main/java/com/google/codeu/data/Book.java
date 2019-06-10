91ebeeca3789cf18556d83a0be03cc7b5f8f91ff
jannatbedi <jannatbedi@gmail.com>
2019-06-10 12:25:34 +0800

    Modified Book.java and created Review.java

--- src/main/java/com/google/codeu/data/Book.java
+++ src/main/java/com/google/codeu/data/Book.java
@@ -5,12 +5,13 @@ import java.util.List;
 public class Book {
   private final Int id;
   private String title;
-  private String[] authors;
+  private List<String> authors;
 
   /**Creates a new Book object for every new book.*/
-  public Book(Int id, String title, String[] authors) {
+  public Book(Int id, String title, List<String> authors) {
     this.id = id;
     this.title = title;
     this.authors = authors;
   }
+
 }
---
+++ src/main/java/com/google/codeu/data/Review.java
@@ -0,0 +1,49 @@
+package com.google.codeu.data;
+
+public class Review {
+ private final Int reviewId;
+ private List<Int> ratings;
+ private List<String> comments;
+ private List<String> pictures;
+
+ /** Creates a new Review object for every new review.*/
+ public Review(Int reviewId, List<Int> ratings) {
+   this.reviewId = reviewId;
+   this.ratings = ratings;
+ }
+
+ public Review(Int reviewId, List<Int> ratings, List<String> comments, List<String> pictures) {
+   this.reviewId = reviewId;
+   this.ratings = ratings;
+   this.comments = comments;
+   this.pictures = pictures;
+ }
+
+  public Int getReviewId() {
+    return reviewId;
+  }
+
+  public List<Int> getRatings() {
+    return ratings;
+  }
+
+  public void setRatings(List<Int> ratings) {
+    this.ratings = ratings;
+  }
+
+  public List<String> getComments() {
+    return comments;
+  }
+
+  public void setComments(List<String> comments) {
+    this.comments = comments;
+  }
+
+  public List<String> getPictures() {
+    return pictures;
+  }
+
+  public void setPictures(List<String> pictures) {
+    this.pictures = pictures;
+  }
+}
