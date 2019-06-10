90bf0031b1c6c0e5178679d77d7b29d3e88ae25c
jannatbedi <jannatbedi@gmail.com>
2019-06-10 11:18:44 +0800

    Modified Book.java

--- src/main/java/com/google/codeu/data/Book.java
+++ src/main/java/com/google/codeu/data/Book.java
@@ -13,70 +13,70 @@ public class Book {
 
 	/*Creates a new Book object for every new book.*/
 	public Book(Int id, String title, String[] authors) {
-	  this.id = id;
+		this.id = id;
 		this.title = title;
 		this.authors = authors;
 	}
-//	public Book(Int id, String title, String[] authors, List<Int> ratings, List<String> reviews, String coverPic, List<String> morePics){
-//		this.id = id;
-//		this.title = title;
-//		this.authors = authors;
-//		this.ratings = ratings;
-//		this.reviews = reviews;
-//		this.coverPic = coverPic;
-//		this.morePics = morePics;
-//	}
+	public Book(Int id, String title, String[] authors, List<Int> ratings, List<String> reviews, String coverPic, List<String> morePics){
+		this.id = id;
+		this.title = title;
+		this.authors = authors;
+		this.ratings = ratings;
+		this.reviews = reviews;
+		this.coverPic = coverPic;
+		this.morePics = morePics;
+	}
 
 	/*Getters and Setters*/
 	public Int getId() {
 		return id;
 	}
-//
-//	public String getTitle() {
-//		return title;
-//	}
-//
-//	public void setTitle(String title) {
-//		this.title = title;
-//	}
-//
-//	public String[] getAuthors() {
-//		return authors;
-//	}
-//
-//	public void setAuthors(String[] authors) {
-//		this.authors = authors;
-//	}
-//
-//	public List<Int> getRatings() {
-//		return ratings;
-//	}
-//
-//	public void setRatings(List<Int> ratings) {
-//		this.ratings = ratings;
-//	}
-//
-//	public List<String> getReviews() {
-//		return reviews;
-//	}
-//
-//	public void setReviews(List<String> reviews) {
-//		this.reviews = reviews;
-//	}
-//
-//	public String getCoverPic() {
-//		return coverPic;
-//	}
-//
-//	public void setCoverPic(String coverPic) {
-//		this.coverPic = coverPic;
-//	}
-//
-//	public List<String> getMorePics() {
-//		return morePics;
-//	}
-//
-//	public void setMorePics(List<String> morePics) {
-//		this.morePics = morePics;
-//	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String[] getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(String[] authors) {
+		this.authors = authors;
+	}
+
+	public List<Int> getRatings() {
+		return ratings;
+	}
+
+	public void setRatings(List<Int> ratings) {
+		this.ratings = ratings;
+	}
+
+	public List<String> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<String> reviews) {
+		this.reviews = reviews;
+	}
+
+	public String getCoverPic() {
+		return coverPic;
+	}
+
+	public void setCoverPic(String coverPic) {
+		this.coverPic = coverPic;
+	}
+
+	public List<String> getMorePics() {
+		return morePics;
+	}
+
+	public void setMorePics(List<String> morePics) {
+		this.morePics = morePics;
+	}
 }
