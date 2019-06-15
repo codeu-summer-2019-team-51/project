5024d1dcccc2e995d14a37cd695bac9b93ae01ec
Karen Frilya Celine <karenfrilya97@gmail.com>
2019-06-15 15:46:33 +0800

    Add Servlet logic for Book List

--- src/main/java/com/google/codeu/servlets/BookListServlet.java
+++ src/main/java/com/google/codeu/servlets/BookListServlet.java
@@ -1,5 +1,9 @@
 package com.google.codeu.servlets;
 
+import com.google.codeu.data.Book;
+import com.google.codeu.data.Datastore;
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -9,16 +13,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Handles fetching all books for the book list.
+ * Handles fetching of all books for the book list.
  */
 
 @WebServlet("/book-list")
 public class BookListServlet extends HttpServlet {
 
+  private Datastore datastore;
+
+  @Override
+  public void init() {
+    datastore = new Datastore();
+  }
+
+  /**
+  * Responds with a JSON representation of a list of Book objects.
+  */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException {
 
-    response.getOutputStream().println("this will be my book list");
+    response.setContentType("application/json");
+
+    List<Book> books = datastore.getAllBooks();
+    Gson gson = new Gson();
+    String json = gson.toJson(books);
+
+    response.getOutputStream().println(json);
   }
 }
