2bab63e9008394a6b91a515bc743ed9676f97b10
Karen Frilya Celine <karenfrilya97@gmail.com>
2019-06-15 16:24:33 +0800

    Add book-list.html

---
+++ src/main/webapp/js/book-list.js
@@ -0,0 +1,47 @@
+function buildBookDiv(book) {
+  const titleDiv = document.createElement('div');
+  titleDiv.classList.add('left-align');
+  titleDiv.appendChild(document.createTextNode(book.title));
+
+  const authorsDiv = document.createElement('div');
+  authorsDiv.classList.add('left-align');
+  authorsDiv.appendChild(document.createTextNode(book.authors.join()));
+
+  const headerDiv = document.createElement('div');
+  headerDiv.classList.add('message-header');
+  headerDiv.appendChild(titleDiv);
+
+  const bodyDiv = document.createElement('div');
+  bodyDiv.classList.add('message-body');
+  bodyDiv.appendChild(authorsDiv);
+
+  const bookDiv = document.createElement('div');
+  bookDiv.classList.add('message-div');
+  bookDiv.appendChild(headerDiv);
+  bookDiv.appendChild(bodyDiv);
+
+  return bookDiv;
+}
+
+// Fetch books and add them to the page.
+function fetchBooks() {
+  const url = '/book-list';
+  fetch(url).then(response => response.json())
+    .then((books) => {
+      const bookContainer = document.getElementById('book-container');
+      if (books.length === 0) {
+        bookContainer.innerHTML = '<p>There are no books yet.</p>';
+      } else {
+        bookContainer.innerHTML = '';
+      }
+      books.forEach((book) => {
+        const bookDiv = buildBookDiv(book);
+        bookContainer.appendChild(bookDiv);
+      });
+    });
+}
+
+// Fetch data and populate the UI of the page.
+function buildUI() { // eslint-disable-line no-unused-vars
+  fetchBooks();
+}
