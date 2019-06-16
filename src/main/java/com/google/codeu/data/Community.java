e6a084b712f0a7cca3f2e5d714a1285d2eabdd18
Karen Frilya Celine <karenfrilya97@gmail.com>
2019-06-16 15:20:26 +0800

    Add Forum related classes: Community, Thread and Post

---
+++ src/main/java/com/google/codeu/data/Community.java
@@ -0,0 +1,81 @@
+package com.google.codeu.data;
+
+import com.google.codeu.data.Thread;
+import com.google.codeu.data.User;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+public class Community {
+
+  private final UUID id;
+  private String name;
+  private String description;
+  private List<User> members;
+  private List<Thread> threads;
+  //TODO: add moderators with more permissions than members
+
+  /**
+   * Constructs a new {@link Community} called {@code name} created by
+   * {@code creator}. Generates a random ID.
+   */
+  public Community(String name, String description, User creator) {
+    this(UUID.randomUUID(), name, description,
+        (List<User>) Arrays.asList(creator),
+        (List<Thread>) new ArrayList<Thread>());
+  }
+
+  /**
+   * Constructs a new {@link Community} with all its attributes filled. It is
+   * used to create a {@link Community} based on {@link Entity} stored in
+   * {@link Datastore}.
+   */
+  public Community(UUID id, String name, String description, List<User> members,
+      List<Thread> threads) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.members = members;
+    this.threads = threads;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public List<User> getMembers() {
+    return members;
+  }
+
+  // Should we have a setMembers method or addMember and deleteMember methods?
+  public void setMembers(List<User> members) {
+    this.members = members;
+  }
+
+  public List<Thread> getThreads() {
+    return threads;
+  }
+
+  // Should we have a setThreads method or addThread and deleteThread methods?
+  public void setThreads(List<Thread> threads) {
+    this.threads = threads;
+  }
+}
