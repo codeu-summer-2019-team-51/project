4032fbb50eaca4f53c2a8a6873671d4c8c027ca7
Karen Frilya Celine <karenfrilya97@gmail.com>
2019-06-16 21:40:56 +0800

    Add Servlet logic for creating new Community

--- src/main/java/com/google/codeu/servlets/ForumServlet.java
+++ src/main/java/com/google/codeu/servlets/ForumServlet.java
@@ -1,5 +1,7 @@
 package com.google.codeu.servlets;
 
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 import com.google.codeu.data.Community;
 import com.google.codeu.data.Datastore;
 import com.google.codeu.data.User;
@@ -12,6 +14,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
 
 /**
  * Handles fetching forum contents.
@@ -40,5 +45,26 @@ public class ForumServlet extends HttpServlet {
     String json = gson.toJson(communities);
 
     response.getOutputStream().println(json);
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+
+    UserService userService = UserServiceFactory.getUserService();
+    if (!userService.isUserLoggedIn()) {
+      response.sendRedirect("/index.html");
+      return;
+    }
+
+    String userEmail = userService.getCurrentUser().getEmail();
+    String name = Jsoup.clean(request.getParameter("name"), Whitelist.none());
+    String description = Jsoup.clean(request.getParameter("description"),
+        Whitelist.none());
+
+    Community community = new Community(name, description, userEmail);
+    datastore.storeCommunity(community);
+
+    response.sendRedirect("/forum.html");
   }
 }
