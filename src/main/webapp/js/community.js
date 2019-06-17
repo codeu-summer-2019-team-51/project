b449ad70f21c6a4108dc7db5f92e3ef263507705
Karen Frilya Celine <karenfrilya97@gmail.com>
2019-06-17 23:25:13 +0800

    Add Community page that can be opened from the community name in Forum page

---
+++ src/main/webapp/js/community.js
@@ -0,0 +1,69 @@
+function getCommunityId() {
+  let params = window.location.search.substr(1);
+  params = params.split("&");
+  for (const element of params) {
+    const param = element.split("=");
+    if (param[0] = 'id') {
+      return param[1];
+    }
+  }
+  return '';
+}
+
+function buildThreadDiv(thread) {
+  const nameDiv = document.createElement('div');
+  nameDiv.classList.add('left-align');
+  nameDiv.classList.add('thread-name');
+  nameDiv.appendChild(document.createTextNode(thread.name));
+
+  const descriptionDiv = document.createElement('div');
+  descriptionDiv.classList.add('left-align');
+  descriptionDiv.classList.add('thread-description');
+  descriptionDiv.appendChild(document.createTextNode(thread.description));
+
+  const threadA = document.createElement('a');
+  threadA.classList.add('thread-a');
+  threadA.appendChild(nameDiv);
+  threadA.appendChild(descriptionDiv);
+
+  return threadA;
+}
+
+// Fetch threads and add them to the page.
+function fetchThreads(id) {
+  const url = `/community?id=${id}`;
+  fetch(url).then(response => response.json())
+    .then((threads) => {
+      const threadContainer = document.getElementById('thread-container');
+      if (threads.length === 0) {
+        threadContainer.innerHTML = '<p>There are no threads yet.</p>';
+      } else {
+        threadContainer.innerHTML = '';
+      }
+      threads.forEach((thread) => {
+        const threadA = buildThreadDiv(thread);
+        threadContainer.appendChild(threadA);
+      });
+    });
+}
+
+/**
+ * Shows if the user is logged in.
+ */
+function showIfLoggedIn() {
+  fetch('/login-status')
+    .then(response => response.json())
+    .then((loginStatus) => {
+      if (loginStatus.isLoggedIn) {
+        document.getElementById('create-button').classList.remove('hidden');
+      }
+    });
+}
+
+// Fetch data and populate the UI of the page.
+function buildUI() { // eslint-disable-line no-unused-vars
+  const id = getCommunityId();
+
+  showIfLoggedIn();
+  fetchThreads(id);
+}
