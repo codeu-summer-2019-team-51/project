d0acbf1d4ea4a2bf4b89c331792e19dadbff2467
jannatbedi <jannatbedi@gmail.com>
2019-06-18 12:06:28 +0800

    Added addBook

--- src/main/java/com/google/codeu/servlets/AboutBookServlet.java
+++ src/main/java/com/google/codeu/servlets/AboutBookServlet.java
@@ -11,6 +11,7 @@ import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.codeu.data.Datastore;
 import com.google.codeu.data.Book;
+
 import java.util.List;
 
 import javax.servlet.annotation.WebServlet;
@@ -19,28 +20,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.util.UUID;
+
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Whitelist;
 
 /**
- *Handles fetching and saving book data.
+ * Handles fetching and saving book data.
  */
 @WebServlet("/aboutbook")
 public class AboutBookServlet extends HttpServlet {
-    
+
   private Datastore datastore;
-  
+
   @Override
   public void init() {
     datastore = new Datastore();
   }
-  
+
   /**
    * Responds with all details for a particular book.
    */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
-          throws IOException {
+      throws IOException {
     response.setContentType("text/html");
     String book = request.getParameter("book");
     if (book == null || book.equals("")) {
@@ -49,39 +51,39 @@ public class AboutBookServlet extends HttpServlet {
     }
     UUID bookId = UUID.fromString(book);
     Book bookData = datastore.getBook(bookId);
-    if(bookData == null) {
+    if (bookData == null) {
       return;
     }
     response.getOutputStream().println(bookData.getTitle());
-    for(int i = 0; i < bookData.getAuthors().size()-1; i++){
+    for (int i = 0; i < bookData.getAuthors().size() - 1; i++) {
       response.getOutputStream().print(bookData.getAuthors().get(i) + ", ");
     }
     response.getOutputStream().print(bookData.getAuthors()
-            .get(bookData.getAuthors().size()-1));
+        .get(bookData.getAuthors().size() - 1));
     response.getOutputStream().println(bookData.getAvgRating());
-    if(bookData.getReviews()!=null){
-      for(int i = 0; i < bookData.getReviews().size(); i++){
-      response.getOutputStream().println("1. " + bookData.getReviews().get(i));
+    if (bookData.getReviews() != null) {
+      for (int i = 0; i < bookData.getReviews().size(); i++) {
+        response.getOutputStream().println("1. " + bookData.getReviews().get(i));
       }
     }
   }
-  
+
   /**
    * Stores a new Book.
    */
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response)
-          throws IOException {
+      throws IOException {
     String title = Jsoup.clean(request.getParameter("title"), Whitelist.none());
     List<String> authors = null;
-    authors.set(0, Jsoup.clean(request.getParameter("authors"), 
-            Whitelist.none()));
-    while(request.getParameter("authors")!=null){
-      authors.add(Jsoup.clean(request.getParameter("authors"), 
-              Whitelist.none()));
+    authors.set(0, Jsoup.clean(request.getParameter("authors"),
+        Whitelist.none()));
+    while (request.getParameter("authors") != null) {
+      authors.add(Jsoup.clean(request.getParameter("authors"),
+          Whitelist.none()));
     }
     Book book = new Book(title, authors);
     datastore.storeBook(book);
-    response.sendRedirect("/aboutbook.html?book=" + book );
+    response.sendRedirect("/aboutbook.html?book=" + book);
   }
 }
