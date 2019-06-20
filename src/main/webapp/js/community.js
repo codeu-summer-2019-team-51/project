d7d0329b3bbc836b06cad89c333d41c46596622e
jannatbedi <jannatbedi@gmail.com>
2019-06-20 09:52:38 +0800

    Modified community.js

--- src/main/webapp/js/community.js
+++ src/main/webapp/js/community.js
@@ -1,12 +1,14 @@
-/*Fetches users and adds them to the page.*/
-function fetchUserList(){
-  const url='/user-list';
-  fetch(url).then((response)=>{
+/**
+*Fetches users and adds them to the page.
+*/
+function fetchUserList() {
+  const url = '/user-list';
+  fetch(url).then((response) => {
     return response.json();
-  }).then((users)=>{
-    const list =document.getElementById('list');
-  	list.innerHTML='';
-    users.forEach((user)=>{
+  }).then((users) => {
+    const list = document.getElementById('list');
+  	list.innerHTML = '';
+    users.forEach((user) => {
       const userListItem = buildUserListItem(user);
   	  list.appendChild(userListItem);
     });
@@ -17,11 +19,11 @@ function fetchUserList(){
 *Builds a list element that contains a link to a user page, e.g.
 *<li><a href="/user-page.html?user=test@example.com">test@example.com</a></li>.
 */
-function buildUserListItem(user){
+function buildUserListItem(user) {
   const userLink = document.createElement('a');
   userLink.setAttribute('href','/user-page.html?user='+user);
   userLink.appendChild(document.createTextNode(user));
-  const userListItem=document.createElement('li');
+  const userListItem = document.createElement('li');
   userListItem.appendChild(userLink);
   return userListItem;
 }
@@ -29,6 +31,6 @@ function buildUserListItem(user){
 /**
 *Fetches data and populates the UI of the page.
 */
-function buildUI(){
+function buildUI() {
   fetchUserList();
 }
