259c86d548437a0bc138e8e8bc20e1a465ef0a66
Karen Frilya Celine <karenfrilya97@gmail.com>
2019-06-24 22:55:22 +0800

    Add Community page that can be accessed from Forum (#29)
    * Add ForumServlet
    
    * Add Forum related classes: Community, Thread and Post
    
    * Add storeCommunity and getAllCommunities methods in Datastore
    
    * Add forum.html page
    
    * Remove threads from attributes of Community
    
    * Add Create new community modal in forum.html
    
    * Only show Create new community button if user is logged in
    
    * Add Servlet logic for creating new Community
    
    * Fix lint error
    
    * Change Thread property to make it easier to store in Datastore
    
    * Add CommunityServlet
    
    * Add CommunityServlet
    
    * Add Datastore logic for community page
    
    * Fix bugs from previous commit
    
    * Add Community page that can be opened from the community name in Forum page
    
    * Add Create new thread button in Community page
    
    * Refactor entityToCommunity and entityToThread methods
    
    * Make CommunityServlet pass Community object and a list of Threads as a response to a POST request
    
    * Display community details on community.html page
    
    * Fix lint errors
    
    * Fix lint errors
    
    * Fix lint errors
    
    * Integrate Milligram in community.html
    
    * Fix lint errors
    
    * Fix lint errors
    
    * Change Javadoc comment verbs from Gets to Returns
    
    * Make minor edit to comment to standardise
    
    * Fix lint errors

---
+++ src/main/webapp/js/community.js
@@ -0,0 +1,96 @@
+function getCommunityId() {
+  let params = window.location.search.substr(1);
+  params = params.split('&');
+  for (const element of params) { // eslint-disable-line no-restricted-syntax
+    const param = element.split('=');
+    if (param[0] === 'id') {
+      return param[1];
+    }
+  }
+  return '';
+}
+
+function buildThreadDiv(thread) {
+  const nameDiv = document.createElement('h3');
+  nameDiv.classList.add('left-align');
+  nameDiv.classList.add('thread-name');
+  nameDiv.appendChild(document.createTextNode(thread.name));
+
+  const descriptionDiv = document.createElement('div');
+  descriptionDiv.classList.add('left-align');
+  descriptionDiv.classList.add('thread-description');
+  descriptionDiv.appendChild(document.createTextNode(thread.description));
+
+  const threadA = document.createElement('a');
+  threadA.classList.add('thread-a');
+  threadA.appendChild(nameDiv);
+  threadA.appendChild(descriptionDiv);
+  
+  return threadA;
+}
+
+function loadCommunityDetails(community) {
+  const title = document.getElementsByTagName('title')[0];
+  title.innerText = community.name;
+
+  const header = document.getElementById('header');
+  header.innerText = community.name;
+
+  const description = document.getElementById('description');
+  description.innerText = community.description;
+
+  const members = document.getElementById('members');
+  const memberCount = community.members.length;
+  members.innerHTML = memberCount > 1
+    ? `<b>${memberCount} members</b>`
+    : `<b>${memberCount} member</b>`;
+}
+
+function loadThreads(threads) {
+  const threadContainer = document.getElementById('thread-container');
+  if (threads.length === 0) {
+    threadContainer.innerHTML = '<p>There are no threads yet.</p>';
+  } else {
+    threadContainer.innerHTML = '';
+  }
+  threads.forEach((thread) => {
+    const threadA = buildThreadDiv(thread);
+    threadContainer.appendChild(threadA);
+  });
+}
+
+// Fetch community and threads and add them to the page.
+function fetchContent(id) {
+  const url = `/community?id=${id}`;
+  fetch(url).then(response => response.json())
+    .then((result) => {
+      loadCommunityDetails(result.community);
+      loadThreads(result.threads);
+    });
+}
+
+/**
+ * Shows if the user is logged in.
+ */
+function showIfLoggedIn() {
+  fetch('/login-status')
+    .then(response => response.json())
+    .then((loginStatus) => {
+      if (loginStatus.isLoggedIn) {
+        document.getElementById('create-button').classList.remove('hidden');
+      }
+    });
+}
+
+function setPostParam(id) {
+  const communityIdParam = document.getElementById('community-id-param');
+  communityIdParam.setAttribute('value', id);
+}
+
+// Fetch data and populate the UI of the page.
+function buildUI() { // eslint-disable-line no-unused-vars
+  const id = getCommunityId();
+  showIfLoggedIn();
+  fetchContent(id);
+  setPostParam(id);
+}
