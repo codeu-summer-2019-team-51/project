65d835566c504a432a5d780e51300300bf9803f1
jannatbedi <jannatbedi@gmail.com>
2019-06-24 14:33:36 +0800

    Stickler CI Modification

--- src/main/java/com/google/codeu/servlets/AboutBookServlet.java
+++ src/main/java/com/google/codeu/servlets/AboutBookServlet.java
@@ -3,14 +3,14 @@ package com.google.codeu.servlets;
 import com.google.codeu.data.Book;
 import com.google.codeu.data.Datastore;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Whitelist;
 
--- src/main/webapp/js/book-page-loader.js
+++ src/main/webapp/js/book-page-loader.js
@@ -5,7 +5,7 @@ const urlParams = new URLSearchParams(window.location.search);
 const parameterBookname = urlParams.get('book');
 console.log(19);
 console.log(parameterBookname);
-//const parameterBookname = "4a785b44-d3d7-47f7-a936-6c49137cbd53";
+// const parameterBookname = "4a785b44-d3d7-47f7-a936-6c49137cbd53";
 
 // URL must include ?book=XYZ parameter. If not, redirect to homepage.
 if (!parameterBookname) {
@@ -15,7 +15,7 @@ if (!parameterBookname) {
 /** Sets the page title based on the URL parameter bookname. */
 function setPageTitle() {
   document.getElementById('page-title').innerText = parameterBookname;
-  document.title = parameterBookname + ' - Book Page';
+  document.title = parameterBookname + ' - Book Page'; // eslint-disable-line prefer-template
 }
 
 /** Fetches book title and adds it to the page. */
@@ -48,16 +48,15 @@ function fetchBookAuthors() {
     });
 }
 
-//function showData() {
+// function showData() {
 //
 //        show('about-book-form');
 //
-//}
+// }
 
 /** Fetches data and populates the UI of the page. */
-function buildUI() {
+function buildUI() { //eslint-disable-line no-unused-vars
   setPageTitle();
-//  fetchBookTitle();
-//  fetchBookAuthors();
-
+  fetchBookTitle();
+  fetchBookAuthors();
 }
