c7eb66ec77a4fd88ec8a1fd77d8711cc7387046b
Karen Frilya Celine <karenfrilya97@gmail.com>
2019-06-29 14:14:48 +0800

    Add doPost method in ThreadServlet

--- src/main/java/com/google/codeu/servlets/ThreadServlet.java
+++ src/main/java/com/google/codeu/servlets/ThreadServlet.java
@@ -1,5 +1,7 @@
 package com.google.codeu.servlets;
 
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 import com.google.codeu.common.Tree;
 import com.google.codeu.data.Comment;
 import com.google.codeu.data.Datastore;
@@ -13,6 +15,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
 
 /**
 * Handles fetching thread comments.
@@ -47,5 +52,26 @@ public class ThreadServlet extends HttpServlet {
         commentsJson);
 
     response.getOutputStream().println(resultJson);
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, IllegalArgumentException {
+
+    UserService userService = UserServiceFactory.getUserService();
+    if (!userService.isUserLoggedIn()) {
+      response.sendRedirect("/index.html");
+      return;
+    }
+
+    String user = userService.getCurrentUser().getEmail();
+    String text = Jsoup.clean(request.getParameter("text"), Whitelist.none());
+    String parentId = request.getParameter("parentId");
+    String threadId = request.getParameter("threadId");
+
+    Comment comment = new Comment(text, user, parentId, threadId);
+    datastore.storeComment(comment);
+
+    response.sendRedirect("/thread.html?id=" + threadId);
   }
 }
