852108b2597626e8c87f4b67ed1ee55c4fa672c4
archthegit <archieprad@gmail.com>
2019-06-30 11:06:09 +0200

    resolving stickler #1

--- src/main/java/com/google/codeu/data/Review.java
+++ src/main/java/com/google/codeu/data/Review.java
@@ -5,97 +5,97 @@ import java.util.List;
 import java.util.UUID;
 
 public class Review {
-private final UUID reviewId; //This is the ID of the review
-private final long timestamp;
-private String author; //The user posting the review
-private long rating;
-private String comment;
-private List<String> pictures;
-private final String bookId;
+  private final UUID reviewId; //This is the ID of the review
+  private final long timestamp;
+  private String author; //The user posting the review
+  private long rating;
+  private String comment;
+  private List<String> pictures;
+  private final String bookId;
 
-/**
- * Creates a new Review object for every new review for cases when
- * the user wants to provide only a rating.
- */
-public Review(String author, long rating, String bookId) {
-  this(UUID.randomUUID(), System.currentTimeMillis(), author, rating, "",
+  /**
+   * Creates a new Review object for every new review for cases when
+   * the user wants to provide only a rating.
+   */
+  public Review(String author, long rating, String bookId) {
+    this(UUID.randomUUID(), System.currentTimeMillis(), author, rating, "",
+        new ArrayList<String>(), bookId);
+  }
+
+  /**
+   * Creates a new Review object for every new review for cases when
+   * the user wants to provide a review without any pictures.
+   */
+  public Review(String author, long rating, String comment, String bookId) {
+    this(UUID.randomUUID(), System.currentTimeMillis(), author, rating, comment,
       new ArrayList<String>(), bookId);
+  }
+
+  /**
+   * Creates a new Review object for every new review for cases when
+   * the user wants to provide a complete review.
+   */
+  public Review(String author, long rating, String comment,
+                List<String> pictures, String bookId) {
+    this(UUID.randomUUID(), System.currentTimeMillis(), author, rating, comment,
+        pictures, bookId);
 }
 
-/**
- * Creates a new Review object for every new review for cases when
- * the user wants to provide a review without any pictures.
- */
-public Review(String author, long rating, String comment, String bookId) {
-  this(UUID.randomUUID(), System.currentTimeMillis(), author, rating, comment,
-      new ArrayList<String>(), bookId);
-}
+  /**
+   * Constructor method to retrieve existing data from datastore.
+   */
+  public Review(UUID reviewId, long timestamp, String author, long rating,
+                String comment, List<String> pictures, String bookId) {
+    this.reviewId = reviewId;
+    this.author = author;
+    this.rating = rating;
+    this.comment = comment;
+    this.pictures = pictures;
+    this.timestamp = timestamp;
+    this.bookId = bookId;
+  }
 
-/**
- * Creates a new Review object for every new review for cases when
- * the user wants to provide a complete review.
- */
-public Review(String author, long rating, String comment,
-              List<String> pictures, String bookId) {
-  this(UUID.randomUUID(), System.currentTimeMillis(), author, rating, comment,
-      pictures, bookId);
-}
-
-/**
- * Constructor method to retrieve existing data from datastore.
- */
-public Review(UUID reviewId, long timestamp, String author, long rating,
-              String comment, List<String> pictures, String bookId) {
-  this.reviewId = reviewId;
-  this.author = author;
-  this.rating = rating;
-  this.comment = comment;
-  this.pictures = pictures;
-  this.timestamp = timestamp;
-  this.bookId = bookId;
-}
-
-public UUID getId() {
+  public UUID getId() {
   return reviewId;
 }
 
-public long getTimestamp() {
+  public long getTimestamp() {
   return timestamp;
 }
 
-public String getAuthor() {
+  public String getAuthor() {
   return author;
 }
 
-public void setAuthor(String author) {
+  public void setAuthor(String author) {
   this.author = author;
 }
 
-public long getRating() {
+  public long getRating() {
   return rating;
 }
 
-public void setRating(long rating) {
+  public void setRating(long rating) {
   this.rating = rating;
 }
 
-public String getComment() {
+  public String getComment() {
   return comment;
 }
 
-public void setComment(String comment) {
+  public void setComment(String comment) {
   this.comment = comment;
 }
 
-public List<String> getPictures() {
+  public List<String> getPictures() {
   return pictures;
 }
 
-public void setPictures(List<String> pictures) {
+  public void setPictures(List<String> pictures) {
   this.pictures = pictures;
 }
 
-public String getBookId() {
+  public String getBookId() {
   return bookId;
 }
 }
