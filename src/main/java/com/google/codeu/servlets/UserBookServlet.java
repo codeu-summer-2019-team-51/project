a286f10def5d9096b276c0972cea212f96cae97e
Karen Frilya Celine <karenfrilya97@gmail.com>
2019-07-02 22:27:09 +0800

    Add UserBookServlet

---
+++ src/main/java/com/google/codeu/servlets/UserBookServlet.java
@@ -0,0 +1,55 @@
+package com.google.codeu.servlets;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.codeu.data.Book;
+import com.google.codeu.data.Datastore;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Handles fetching of books saved by user.
+ */
+@WebServlet("/user-book")
+public class UserBookServlet extends HttpServlet {
+
+  private Datastore datastore;
+
+  @Override
+  public void init() {
+    datastore = new Datastore();
+  }
+
+  /**
+  * Responds with a JSON representation of a list of Book objects saved by
+  * {@code user} with reading status {@code status}.
+  */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+
+    response.setContentType("application/json");
+
+    UserService userService = UserServiceFactory.getUserService();
+    if (!userService.isUserLoggedIn()) {
+      response.sendRedirect("/index.html");
+      return;
+    }
+
+    String user = userService.getCurrentUser().getEmail();
+    String status = request.getParameter("status");
+
+    List<Book> books = datastore.getUserBooks(user, status);
+    Gson gson = new Gson();
+    String json = gson.toJson(books);
+
+    response.getOutputStream().println(json);
+  }
+}
