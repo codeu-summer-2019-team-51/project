e6d93da2f3ffe150eb7d05b16316668e5b16804e
jannatbedi <jannatbedi@gmail.com>
2019-07-08 21:33:31 +0800

    Made the requested changes

--- src/main/webapp/js/user-list.js
+++ src/main/webapp/js/user-list.js
@@ -4,7 +4,7 @@
 */
 function buildUserListItem(user) {
   const userLink = document.createElement('a');
-  userLink.setAttribute('href', '/user-page.html?user=${user}'); 
+  userLink.setAttribute('href','/user-page.html?user=${user}'); 
   userLink.appendChild(document.createTextNode(user));
   const userListItem = document.createElement('li');
   userListItem.appendChild(userLink);
