c3154370b0b53ad95e7b250517ea845029697f99
Karen Frilya Celine <karenfrilya97@gmail.com>
2019-07-30 00:58:19 +0800

    Edit backend logic for storing profilePic in Datastore and rename UserListServlet to UserServlet which returns either a list of users or a single user depending on if user parameter is provided

---
+++ src/main/java/com/google/codeu/servlets/UserServlet.java
@@ -0,0 +1,69 @@
+package com.google.codeu.servlets;
+
+import com.google.codeu.data.Datastore;
+import com.google.codeu.data.User;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*Handles fetching all users for the community page*/
+
+@WebServlet("/users")
+public class UserServlet extends HttpServlet {
+
+  private Datastore datastore;
+
+  @Override
+  public void init() {
+    datastore = new Datastore();
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+
+    response.setContentType("application/json");
+
+    String user = request.getParameter("user");
+    if (user != null) {
+      // Return only userData of specified user
+
+      if (user.equals("")) {
+        // Request is invalid, return empty response
+        return;
+      }
+
+      User userData = datastore.getUser(user);
+      Gson gson = new Gson();
+      String json = gson.toJson(userData);
+      response.getOutputStream().println(json);
+
+    } else {
+      // Return all userData
+      Set<String> userEmailsWithReviews = datastore.getUserEmailsWithReviews();
+      Map<String, User> allUserData = datastore.getAllUsers();
+
+      List<User> usersWithReviews = new ArrayList<User>();
+      for (String userEmail : userEmailsWithReviews) {
+        User userData = allUserData.get(userEmail);
+        if (userData == null) {
+          userData = new User(userEmail);
+        }
+        usersWithReviews.add(userData);
+      }
+
+      Gson gson = new Gson();
+      String json = gson.toJson(usersWithReviews);
+      response.getOutputStream().println(json);
+    }
+  }
+}
--- src/main/webapp/js/user-list.js
+++ src/main/webapp/js/user-list.js
@@ -3,11 +3,20 @@
 *<li><a href="/user-page.html?user=test@example.com">test@example.com</a></li>.
 */
 function buildUserListItem(user) {
+  // const profilePic = document.createElement('img');
+  // let filePath = user.profilePic;
+  // if (!filePath) {
+  //   filePath = 'image/profile-pic.png';
+  // }
+  // profilePic.src = filePath;
+
   const userLink = document.createElement('a');
-  const temp = `/user-page.html?user=${user}`;
+  const temp = `/user-page.html?user=${user.email}`;
   userLink.setAttribute('href', temp);
-  userLink.appendChild(document.createTextNode(user));
+  userLink.appendChild(document.createTextNode(user.email));
+
   const userListItem = document.createElement('li');
+  // userListItem.appendChild(profilePic);
   userListItem.appendChild(userLink);
   return userListItem;
 }
@@ -16,13 +25,13 @@ function buildUserListItem(user) {
 *Fetches users and adds them to the page.
 */
 function fetchUserList() {
-  const url = '/user-list';
+  const url = '/users';
   fetch(url).then(response => response.json())
     .then((users) => {
       const list = document.getElementById('users-container');
       list.innerHTML = '';
       users.forEach((user) => {
-        const userListItem = buildUserListItem(user.email);
+        const userListItem = buildUserListItem(user);
         list.appendChild(userListItem);
       });
     });
