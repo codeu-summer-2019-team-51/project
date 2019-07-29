201136121068b8adf42fe16f23beb33d545445af
Karen Frilya Celine <karenfrilya97@gmail.com>
2019-07-30 01:41:08 +0800

    Show profile picture and about me in user list

--- src/main/webapp/js/user-list.js
+++ src/main/webapp/js/user-list.js
@@ -3,21 +3,32 @@
 *<li><a href="/user-page.html?user=test@example.com">test@example.com</a></li>.
 */
 function buildUserListItem(user) {
-  // const profilePic = document.createElement('img');
-  // let filePath = user.profilePic;
-  // if (!filePath) {
-  //   filePath = 'image/profile-pic.png';
-  // }
-  // profilePic.src = filePath;
+  const profilePic = document.createElement('img');
+  let filePath = user.profilePic;
+  if (!filePath) {
+    filePath = 'image/profile-pic.png';
+  }
+  profilePic.src = filePath;
+  profilePic.classList.add('profile-pic');
 
   const userLink = document.createElement('a');
-  const temp = `/user-page.html?user=${user.email}`;
-  userLink.setAttribute('href', temp);
+  userLink.href = `/user-page.html?user=${user.email}`;
   userLink.appendChild(document.createTextNode(user.email));
+  userLink.classList.add('user-link');
+
+  const userAboutMe = document.createElement('p');
+  userAboutMe.innerText = user.aboutMe;
+  userAboutMe.classList.add('about-me');
+
+  const userDiv = document.createElement('div');
+  userDiv.classList.add('user-div');
+  userDiv.appendChild(userLink);
+  userDiv.appendChild(userAboutMe);
 
   const userListItem = document.createElement('li');
-  // userListItem.appendChild(profilePic);
-  userListItem.appendChild(userLink);
+  userListItem.classList.add('user');
+  userListItem.appendChild(profilePic);
+  userListItem.appendChild(userDiv);
   return userListItem;
 }
 
@@ -28,7 +39,7 @@ function fetchUserList() {
   const url = '/users';
   fetch(url).then(response => response.json())
     .then((users) => {
-      const list = document.getElementById('users-container');
+      const list = document.getElementById('user-container');
       list.innerHTML = '';
       users.forEach((user) => {
         const userListItem = buildUserListItem(user);
