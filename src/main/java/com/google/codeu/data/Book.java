8085c2bcbe14f9eacc9a7cccf01b0e6be22ce110
jannatbedi <jannatbedi@gmail.com>
2019-08-02 15:58:04 +0800

    Stickler CI modifications

--- src/main/java/com/google/codeu/data/Book.java
+++ src/main/java/com/google/codeu/data/Book.java
@@ -1,9 +1,9 @@
 package com.google.codeu.data;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 enum Tags {
